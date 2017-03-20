6c5e2d7a2712001d990a52c206464a6631d06bb8
jengm22 <mahtarrjeng@gmail.com>
2017-03-20 18:12:23 +0000

    Login Files
    Login code and fxml file added

---
+++ LoginController.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package essproject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class LoginController implements Initializable {
+    
+    @FXML 
+    private Label lblStatus;
+    
+    @FXML
+    private TextField txtUserName;
+    
+    @FXML
+    private TextField txtPassword; 
+   
+    @FXML
+    public void Login(ActionEvent event)throws IOException  {  
+           
+        System.out.println(txtUserName);
+        System.out.println(event.toString());
+        
+        if(txtUserName.getText().equals("user")&& txtPassword.getText().equals("pass")){
+            lblStatus.setText("Login Success");
+              Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("Services.fxml"));        
+            Scene scene = new Scene(root);        
+            stage.setScene(scene);
+            stage.show();         
+            ((Node) (event.getSource())).getScene().getWindow().hide();          
+        }
+        else{
+            lblStatus.setText("Login Failed");
+         } 
+       
+    }
+   
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
