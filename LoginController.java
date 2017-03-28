b9c19ac07342d2de3a5feca5897fed152f05d1b0
jengm22 <mahtarrjeng@gmail.com>
2017-03-28 12:53:59 +0100

    Update LoginController.java

--- LoginController.java
+++ LoginController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package essproject;
+package ess_solentproperty;
 
 import java.io.IOException;
 import java.net.URL;
